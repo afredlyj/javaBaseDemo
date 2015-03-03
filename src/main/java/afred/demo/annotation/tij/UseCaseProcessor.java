6d94e203969c571ceedd708aaa0362d91bc417b5
afredlyj <csulyj@gmail.com>
2015-03-03 22:15:04 +0800

    add annotation code from thinking in java

---
+++ src/main/java/afred/demo/annotation/tij/UseCaseProcessor.java
@@ -0,0 +1,33 @@
+package afred.demo.annotation.tij;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Afred on 15/3/3.
+ */
+public class UseCaseProcessor {
+
+    public static void process(List<Integer> useCases, Class<?> cl) {
+        for (Method method : cl.getDeclaredMethods()) {
+            UseCase uc = method.getAnnotation(UseCase.class);
+            if (uc != null) {
+                System.out.println("use case id : " + uc.id() + ", description : " + uc.description());
+                useCases.remove(new Integer(uc.id()));
+            }
+        }
+
+        for (int i : useCases) {
+            System.out.println("Warning : Missing use case " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> useCase = new ArrayList<Integer>();
+        Collections.addAll(useCase, 47, 48, 49, 50);
+        process(useCase, PasswordUtils.class);
+    }
+
+}
